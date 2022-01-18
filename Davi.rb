c76c3bc4c48edebf5a865fd236d870ffd8da8d75
Davi52-ai <your_github_email>
2022-01-18 08:11:45 -0300

    Criado 18/01/2021

---
+++ Davi.rb
@@ -0,0 +1,17 @@
+nome_completo = "Davi Gedoz"
+
+puts nome_completo
+
+nome_completo = nil
+
+numero_telefone = "61996738232"
+
+puts numero_telefone
+
+resposta = nome_completo.nil?
+
+puts resposta
+
+nome = gets
+puts nome_completo
+
