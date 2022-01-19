b48329e1e26b2fe6aac42cff58fbd0e30bad3471
Davi52-ai <davigedoz@gmail.com>
2022-01-19 10:30:20 -0300

    Criado 19/01/2021

---
+++ AbrindoClasses.rb
@@ -0,0 +1,15 @@
+class Carro
+    def acelera
+      puts "acelerando..."
+    end
+end
+
+class Carro
+    def freia 
+        puts "parando..."
+    end
+end
+
+carro = Carro.new
+carro.acelera
+carro.freia
