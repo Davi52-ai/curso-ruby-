a96b9b5ce95e8baa333f766e5694db79ace6c93b
Davi52-ai <davigedoz@gmail.com>
2022-01-19 10:37:51 -0300

    Criado 19/01/2022

---
+++ MonkeyPatching.rb
@@ -0,0 +1,16 @@
+class Carro
+    def acelera 
+      puts "Acelerando"
+    end
+end
+
+carro = Carro.new
+carro.acelera
+
+class Carro
+    def acelera
+        puts "Novo metodo"
+    end
+end
+
+carro.acelera
