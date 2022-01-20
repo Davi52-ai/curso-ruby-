a278e783a2b6f829d75a823adb6c6aa235491bd2
Davi52-ai <davigedoz@gmail.com>
2022-01-20 15:06:41 -0300

    20/01/2022

--- IfeElse.rb
+++ IfeElse.rb
@@ -19,3 +19,20 @@ end
 puts "B > A" unless a > b
 
 puts "A > B" if a > b
+
+
+# Estrutura condicional IF ELSIF ELSE
+
+a = 20
+
+if a = 20 
+    puts "mensagem caso verdade"
+    puts "outra linha caso verdade"
+
+elsif a = 20
+    puts "essa mensagem se caso o a seja 20"
+
+else
+    puts "a nao e igual a 10 e nem a 20"
+end
+    puts "..| fim do programa"
