5227032f32dc37730d2b398f9000806a35ab2641
Davi52-ai <davigedoz@gmail.com>
2022-01-21 10:03:02 -0300

    Criado 21/01/2022

---
+++ Numeros.rb
@@ -0,0 +1,20 @@
+50 
+
+50.class
+
+30948034850348039850394850385209820984.class
+
+Em binário = 0b1010
+
+Em octal = 0o45
+
+Exadecimal = 0xAD123
+
+representação e seperação _ = 1_000_00
+
+1.5
+
+1.5.class
+
+100 + 1.5
+
