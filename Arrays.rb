3a5098ebaa2e3a232c8226bc4c45af863c8d0861
Davi52-ai <davigedoz@gmail.com>
2022-01-21 15:09:03 -0300

    Criado 21/01/2022

---
+++ Arrays.rb
@@ -0,0 +1,15 @@
+lista = []
+lista = Array.new
+lista = [1,2,3,4]
+lista = ["nome", :nome, 1, 2.5]
+lista = %w(conversao de strings para Array)
+
+lista.size
+lista.empty?
+
+lista[0]
+lista[-1]
+lista[-2]
+lista[0] = "Davi"
+lista.push 10
+lista.join ','
