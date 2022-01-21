29dda5bf0eade4c25da5a6e8adfbf560dc7375a9
Davi52-ai <davigedoz@gmail.com>
2022-01-21 15:07:08 -0300

    Criado 21/01/2022

---
+++ OperadoresemStrings.rb
@@ -0,0 +1,15 @@
+"Rob".downcase
+
+"Rob".upcase
+
+"     muito espaço em branco      ".strip
+
+"Davi gedoz"
+nome["Davi"] = "Rob"
+nome
+
+carro = "jeep renegade"
+carro.catalize
+carro.chars
+carro.split
+carro.split("e")
