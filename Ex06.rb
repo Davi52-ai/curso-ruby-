e863f3be6e7f9678935dde241e7f8fb099b618d5
Davi52-ai <davigedoz@gmail.com>
2022-01-25 13:42:08 -0300

    Criado 25/01/2022

---
+++ Ex06.rb
@@ -0,0 +1,21 @@
+tipos_de_pessoas = 10
+x = "Existem #{tipos_de_pessoas} tipos de pessoas."
+binário = "binário"
+não_faça = "não"
+y = "Aqueles que sabem #{binário} e aqueles que #{não_faça} sabem."
+
+puts x
+puts y
+
+puts "Eu disse: #{x}."
+puts "Eu também disse: '#{y}'."
+
+engraçado = false
+avaliação_da_piada = "Essa piada não é tão engraçada?! #{engraçado}"
+
+puts avaliação_da_piada
+
+w = "Este é o lado esquerdo de..."
+e = "uma string com um lado direito."
+
+puts w + e 
