994f5cb1825ca3332a3ea84a90dcb4a2953ff49f
Davi52-ai <davigedoz@gmail.com>
2022-01-25 09:49:17 -0300

    Criado 25/01/2022

---
+++ Ex03.rb
@@ -0,0 +1,23 @@
+puts "Agora vou contar minhas galinhas"
+
+puts "Galinhas #{25 + 30 / 6}"
+puts "Galos #{100 - 25 * 3 % 4}"
+
+puts "Agora vou contar os ovos:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "É verdade que 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "O que é 3 + 2? #{3 + 2}"
+puts "O que é 5 - 7? #{5 - 7}"
+
+puts "Ah, é por isso que é falso"
+
+puts "Que tal um pouco mais."
+
+puts "É maior? #{5 > -2}"
+puts "É maior ou igual? #{5 >= -2}"
+puts "É menor ou igual? #{5 <= -2}"
