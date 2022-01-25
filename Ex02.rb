6058cd5f3c3564c3bb2a317fe4c545e66e3ce32b
Davi52-ai <davigedoz@gmail.com>
2022-01-25 09:17:25 -0300

    Criado 25/01/2022

---
+++ Ex02.rb
@@ -0,0 +1,9 @@
+# Um comentário, para que você possa ler seu programa mais tarde.
+# Qualquer coisa após o # é ignorada pelo ruby.
+
+puts "Eu poderia ter um código assim." # e o comentário depois é ignorado
+
+# Você também pode usar um comentário para "desativar" ou comentar um pedaço de código:
+# puts "Isto não será executado."
+
+puts "Isto será executado." 
