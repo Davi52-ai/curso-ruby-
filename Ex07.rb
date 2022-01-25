3c86a5629955d1e3847869c835dfcd6a11fa4b77
Davi52-ai <davigedoz@gmail.com>
2022-01-25 13:47:58 -0300

    Criado 25/01/2022

---
+++ Ex07.rb
@@ -0,0 +1,21 @@
+puts "Maria teve um cordeirinho."
+puts "Sua lã era branca como #{'neve'}."
+puts "E em todos os lugares que Maria foi."
+puts "." * 10 # o que isso fez?
+
+fim1 = "C"
+fim2 = "h"
+fim3 = "e"
+fim4 = "e"
+fim5 = "s"
+fim6 = "e"
+fim7 = "B"
+fim8 = "u"
+fim9 = "r"
+fim10 = "g"
+fim11 = "e"
+fim12 = "r"
+
+# assista aquele print vs. puts nessa linha o que ele faz?
+print fim1 + fim2 + fim3 + fim4 + fim5 + fim6
+puts fim7 + fim8 + fim9 + fim10 + fim11 + fim12 
