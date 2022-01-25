3891b2ba96d36658f4d61fd5cdc439b0dca60977
Davi52-ai <davigedoz@gmail.com>
2022-01-25 11:34:41 -0300

    Criado 25/01/2022

---
+++ Ex05.rb
@@ -0,0 +1,17 @@
+meu_nome = 'Davi G. Ávila'
+minha_idade = 21 # não é mentira em 2022
+minha_altura = 2 # altura
+meu_peso = 120 # Kg
+meus_olhos = 'Azul'
+meus_dentes = 'brancos'
+meu_cabelo = 'loiro'
+
+puts "Vamos falar sobre #{meu_nome}."
+puts "Ele tem #{minha_altura} metro de altura."
+puts "Ele pesa #{meu_peso} Kg."
+puts "Na verdade, isso não é muito pasado"
+puts "Ele tem olhos #{meus_olhos} e cabelo #{meu_cabelo}."
+puts "Os dentes dele geralmente são #{meus_dentes} dependendo do café."
+
+# esta linha é complicada, tente acertar exatamente
+puts "Se eu adicionar #{minha_idade}, #{minha_altura} e #{meu_peso} recebo #{minha_idade + minha_altura + meu_peso}."
