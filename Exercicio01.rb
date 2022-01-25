64b37b629bee2249ad8b9f3ae246e9e4dfa4cef2
Davi52-ai <davigedoz@gmail.com>
2022-01-25 09:07:51 -0300

    Criado 24/01/2022

---
+++ Exercicio01.rb
@@ -0,0 +1,7 @@
+puts "Olá mundo!"
+puts "Olá de novo"
+puts "Gosto de digitar isso."
+puts "Isso e divertido."
+puts "Eba! Imprimindo."
+puts "Prefiro que você 'não'."
+puts 'Eu "disse" não toque nisso.'
