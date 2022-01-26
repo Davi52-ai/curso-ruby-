f0651513e6f2bdde60607f84a8f6a3bc420d924a
Davi52-ai <davigedoz@gmail.com>
2022-01-26 09:34:18 -0300

    Criado 06/01/2022

---
+++ Ex10.rb
@@ -0,0 +1,15 @@
+gato_malhado = "\tSou tabulado."
+gato_persa = "Estou dividindo\numa linha."
+barra_invertida_gato = "Sou \\ um \\ gato."
+
+gato_gordo = """
+vou fazer uma lista:
+\t* Comida de gato
+\t* Peixes
+\t* Mordida de gato\n\t* Grama
+"""
+
+puts gato_malhado
+puts gato_persa
+puts barra_invertida_gato
+puts gato_gordo
