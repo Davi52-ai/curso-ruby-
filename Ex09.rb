687efb1ce6d2645466a08d5184b8d211e6b8e1ce
Davi52-ai <davigedoz@gmail.com>
2022-01-26 08:54:45 -0300

    Criado 26/01/2022

---
+++ Ex09.rb
@@ -0,0 +1,14 @@
+# Aqui estão algumas coisas novas e estranhas, lembre-se de digitar exatamente 
+
+dias = "Seg Ter Qua Qui Sex Sáb Dom"
+meses = "Jan\nFev\nMar\nAbr\nMaio\nJun\nJul\nAgo"
+
+puts "Aqui estão os dias: #{dias}"
+puts "Aqui estão os meses: #{meses}"
+
+puts %q{
+Tem algo acontecendo aqui.
+Com esta citação estranha.
+Podemos giditar o quanto quisermos.
+Até 4 linha se quisermos, ou 5, ou 6.
+}
