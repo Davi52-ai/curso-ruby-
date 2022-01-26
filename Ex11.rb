710bb4ce6c1423df84010ad0a28d240b2176be89
Davi52-ai <davigedoz@gmail.com>
2022-01-26 09:45:19 -0300

    Criado 26/01/2022

---
+++ Ex11.rb
@@ -0,0 +1,8 @@
+print "Quantos anos você tem?"
+idade = gets.chomp
+print "Qual sua altura?"
+altura = gets.chomp
+print "Quanto você pesa?"
+peso = gets.chomp
+
+puts "Então, você tem #{idade} anos velho, #{altura} Metro, bem alto e #{peso} Kg, bem pesado."
