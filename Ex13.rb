864fba77e745c40e0eacff3a5b653d52de9ae490
Davi52-ai <davigedoz@gmail.com>
2022-01-26 11:01:17 -0300

    26/01/2022

--- Ex13.rb
+++ Ex13.rb
@@ -1,5 +1,9 @@
 primeiro, segundo, terceiro = ARGV
 
+primeiro = gets.chomp
+segundo = gets.chomp
+terceiro = gets.chomp
+
 puts "Minha primeira variável é: #{primeiro}"
 puts "Minha segunda variável é: #{segundo}"
 puts "Minha terceira variável é: #{terceiro}"
