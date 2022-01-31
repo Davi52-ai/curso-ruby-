411216cd523ab728b18456bb1a3c0f061eb6fd2b
Davi52-ai <davigedoz@gmail.com>
2022-01-31 15:55:55 -0300

    Criado 31/01/2022

---
+++ Ex29.rb
@@ -0,0 +1,36 @@
+people = 20 
+cats = 30
+dogs = 15
+
+
+if people < cats
+    puts "Muitos gatos! O mundo está condenado!"
+end
+
+if people > cats
+    puts "Não muitos gatos! O mundo está salvo!"
+end
+
+if people < dogs 
+    puts "O mundo está babando!"
+end
+
+if people > dogs
+    puts "O mundo está seco!"
+end
+
+
+dogs += 5
+
+if people >= dogs
+    puts "As pessoas são maiores ou iguais aos cães."
+end
+
+if people <= dogs
+    puts "As pessoas são menores ou iguais aos cães."
+end
+
+
+if people == dogs 
+    puts "pessoas e cães."
+end
