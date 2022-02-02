bafcadad3a323eb99983eb0f9ff8b48f1ee63bc7
Davigedoz <davigedoz@gamil.com>
2022-02-02 09:12:18 -0300

    Criado 02/02/2022

---
+++ Ex30.rb
@@ -0,0 +1,26 @@
+people = 10
+cars = 5
+trucks = 12
+
+
+if cars > people
+    puts "Devemos lavar os carros."
+elsif cars < people
+    puts "Não devemos lavar os carros."
+else
+    puts "Não podemos decidir."
+end
+
+if trucks > cars 
+    puts "São muitos caminhoẽs."
+elsif trucks < cars
+    puts "Talvez pudéssemos levar os caminhoẽs."
+else 
+    puts "Ainda não podemos decidir."
+end
+
+if people > trucks 
+    puts "Tudo bem, vamos apenas carregar os caminhoẽs."
+else 
+    puts "Tudo bem, vamos ficar em casa então."
+end
